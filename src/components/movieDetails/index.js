0afbd61569aa2d56689f23943df15d1e04b122a4
XiaoFeng Gong <20091570@wit.ie>
2020-10-18 20:37:09 +0100

    Movie Details Page - included production countries

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -51,6 +51,17 @@ export default ({ movie }) => {
           </li>
         ))}
       </ul>
+
+      <ul className="list-group list-group-horizontal">
+        <li key="pcd" className="list-group-item list-group-item-dark">
+          Production Countries
+        </li>
+        {movie.production_countries.map(pc => (
+          <li key={pc.iso_3166_1} className="list-group-item">
+            {pc.name}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
