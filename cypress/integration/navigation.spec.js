6514073630942f2ec9c8d78f0ee4021b578a6a5f
XiaoFeng Gong <20091570@wit.ie>
2020-11-01 14:51:27 +0000

    E2E tests - Navigation - Movies Details page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,49 +3,68 @@ const movieId = 497582; // Enola Holmes movie id
 let reviews;
 
 describe("Navigation", () => {
-  before(() => {
-    cy.request(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+    before(() => {
+        cy.request(
+                `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
         "TMDB_KEY"
       )}&language=en-US&include_adult=false&include_video=false&page=1`
-    )
-      .its("body")
-      .then((response) => {
-        movies = response.results;
-      });
-    cy.request(
-      `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${Cypress.env(
+            )
+            .its("body")
+            .then((response) => {
+                movies = response.results;
+            });
+        cy.request(
+                `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${Cypress.env(
         "TMDB_KEY"
       )}`
-    )
-      .its("body")
-      .then((response) => {
-        console.log(response);
-        reviews = response.results;
-      });
-  });
+            )
+            .its("body")
+            .then((response) => {
+                console.log(response);
+                reviews = response.results;
+            });
 
-  describe("From the home page", () => {
-    beforeEach(() => {
-      cy.visit("/");
     });
-    it("should navigate to the movie details page and change browser URL", () => {
-      cy.get(".card").eq(1).find("img").click();
-      cy.url().should("include", `/movies/${movies[1].id}`);
-      cy.get("h2").contains(movies[1].title);
+
+    describe("From the home page", () => {
+        beforeEach(() => {
+            cy.visit("/");
+        });
+        it("should navigate to the movie details page and change browser URL", () => {
+            cy.get(".card").eq(1).find("img").click();
+            cy.url().should("include", `/movies/${movies[1].id}`);
+            cy.get("h2").contains(movies[1].title);
+        });
+        it("should allow navigation from site header", () => {
+            cy.get("nav").find("li").eq(2).find("a").click();
+            cy.url().should("include", `/favorites`);
+            cy.get("h2").contains("Favorite Movies");
+            cy.get("nav").find("li").eq(0).find("a").click();
+            cy.url().should("not.include", `/favorites`);
+            cy.get("h2").contains("Discover Movies");
+            cy.get("nav").find("li").eq(2).find("a").click();
+            cy.get("nav.navbar-brand").find("a").click();
+            cy.url().should("not.include", `/favorites`);
+            cy.get("h2").contains("Discover Movies");
+        });
     });
-    it.only("should allow navigation from site header", () => {
-      cy.get("nav").find("li").eq(2).find("a").click();
-      cy.url().should("include", `/favorites`);
-      cy.get("h2").contains("Favorite Movies");
-      cy.get("nav").find("li").eq(0).find("a").click();
-      cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("Discover Movies");
-      cy.get("nav").find("li").eq(2).find("a").click();
-      cy.get("nav.navbar-brand").find("a").click();
-      cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("Discover Movies");
+
+    describe("From the Movie Details page ", () => {
+        beforeEach(() => {
+            cy.visit(`/movies/${movieId}`);
+        });
+        it("should change browser URL when show/hide reviews is clicked", () => {
+            cy.contains("Show Reviews").click();
+            cy.url().should("include", `/movies/${movieId}/reviews`);
+            cy.contains("Hide Reviews").click();
+            cy.url().should("not.include", `/movies/${movieId}/reviews`);
+        });
+        it("navigate to the full review page when a 'Full Review' link is clicked", () => {
+            cy.contains("Show Reviews").click();
+            cy.contains("Full Review").click();
+            cy.url().should("include",`reviews/${reviews[0].id}`)
+        });
     });
-  });
+
 
 });
