67cfadac255b82dec1c133e291e299bd3c5473f6
XiaoFeng Gong <20091570@wit.ie>
2020-11-04 20:44:17 +0000

    Change upcoming movies page to use context

--- src/pages/UpcomingPage.js
+++ src/pages/UpcomingPage.js
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { getUpcomingMovies } from "../api/tmdb-api";
+import React, {useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
+import {MoviesContext} from '../contexts/moviesContext'
 import  AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const UpcomingPage = () => {
-
-    const [movies, setMovies] = useState([]);
-     useEffect(() => {
-      getUpcomingMovies().then(movies => {
-        setMovies(movies);
-      });
-    }, []);
+  const context = useContext(MoviesContext);
+  const movies = context.upcoming;
     return (
         <PageTemplate
           title='Upcoming Movies'
