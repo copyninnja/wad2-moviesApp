345d9a0daff9272e746c04c903d7ff3d1991bbdd
XiaoFeng Gong <20091570@wit.ie>
2020-11-04 17:29:16 +0000

    New button for upcoming movies page

--- src/pages/UpcomingPage.js
+++ src/pages/UpcomingPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
-import AddToFavoritesButton from '../components/buttons/addToFavorites'
+import  AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const UpcomingPage = () => {
 
@@ -16,7 +16,7 @@ const UpcomingPage = () => {
           title='Upcoming Movies'
           movies={movies}
           action={(movie) => {
-            return <AddToFavoritesButton movie={movie} />;
+            return <AddToWatchListButton movie={movie} />;
           }}
         />      
 
