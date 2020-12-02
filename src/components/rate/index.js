9e9a7d9d3f55bffea283161b5cf64f9c1b2d6ca3
XiaoFeng Gong <20091570@wit.ie>
2020-12-02 00:21:46 +0000

    add DELETE request

--- src/components/rate/index.js
+++ src/components/rate/index.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Rate } from 'antd';
 import 'antd/dist/antd.css';
 
 const Rank = ({movie,session_id}) => {
     const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
-    let rank=0;
+    const [rank,setRank]=useState(0);
 
     const rankit=(it)=>{
+        if(it==0){
+        setRank(0);
+        console.log("hello")
+      // POST request using fetch inside useEffect React hook
+      const requestRank = {
+          method: 'DELETE',
+      };
+      fetch(`https://api.themoviedb.org/3/movie/${movie.id}/rating?api_key=${process.env.REACT_APP_TMDB_KEY}&session_id=${session_id}`, requestRank)
+      .then(async response => {
+          const data = await response.json();
+          // check for error response
+          if (!response.ok) {
+              const error = (data && data.message) || response.status;
+              return Promise.reject(error);
+          }
+
+          window.alert(JSON.stringify(data.status_message))
+      })
+      .catch(error => {
+          console.error('There was an error!', error);
+      }); 
+       // eslint-disable-next-line
+    }else{
+        
       let data = new FormData();
+      setRank(it);
       data.append("value",it*2);
       // POST request using fetch inside useEffect React hook
       const requestRank = {
@@ -29,6 +54,7 @@ const Rank = ({movie,session_id}) => {
           console.error('There was an error!', error);
       }); 
        // eslint-disable-next-line
+    }
   }
 
 
@@ -39,7 +65,7 @@ const Rank = ({movie,session_id}) => {
 
 return( 
     <span>
-    <Rate allowClear={true} tooltips={desc} onChange={handleChange} value={rank} data-cy="ratetext" />
+    <Rate  tooltips={desc} onChange={handleChange} value={rank} data-cy="ratetext" />
     {rank ? <span className="ant-rate-text" >{desc[rank - 1]}</span> : ''}
   </span>
 );
