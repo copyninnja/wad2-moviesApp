016aa932d50992f1b1e7e12f38ffd26843c7b5ff
XiaoFeng Gong <20091570@wit.ie>
2020-12-02 23:44:00 +0000

    add react-blur component

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import MovieHeader from '../headerMovie'
 import "./moviePage.css";
-
+import Blur from 'react-blur';
 const TemplateMoviePage = ({ movie, children }) => {
-  const style ={
-    bgd:{
-        color: 'rgba(0,0,0,.70)',
-        backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})`,
-        backgroundSize: '100%,100%',
-    }
-
-  }
+  // const style ={
+  //   bgd:{
+  //       color: 'rgba(0,0,0,.70)',
+  //       backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})`,
+  //       backgroundSize: '100%,100%',
+  //   }
+  // }
 
   return (
     <>
-<div style={style.bgd} > 
+{/* <div style={style.bgd} >  */}
+<Blur img={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} blurRadius={10} enableStyles >  
       <MovieHeader movie={movie} />      
       <div className="row" >
         <div className="col-3">
@@ -30,7 +30,8 @@ const TemplateMoviePage = ({ movie, children }) => {
         </div>
         <div className="col-9">{children}</div>
       </div>
-      </div>
+      {/* </div> */}
+      </Blur>
     </>
   );
 };
